138d5c9e6b0f9e4a0bf7d9b63ada013b539e6a7e
Chris <chris.j.thib@gmail.com>
2023-03-28 15:29:53 -0600

    an attempt was made...

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ const express = require("express");
 
 const app = express();
 //All your code goes here
+app.use(express.json());
+const fs = require('fs');
+
+
+app.get("/courses", (req, res) => {
+    res.json(fs.readFile('./database/courses.json', (error, content)=>{
+        if(error) return error;
+        return content;
+    }));
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
